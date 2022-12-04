a4c86c70bcc2962523511db70af2408693d70d00
Emanuel Mineda Carneiro <mineda@gmail.com>
2022-12-04 17:34:31 -0300

    Update DDL.sql

--- DDL.sql
+++ DDL.sql
@@ -37,10 +37,40 @@ create table ant_anotacao (
   foreign key ant_usuario_fk (usr_id) references usr_usuario (usr_id) on delete restrict on update cascade
 );
 
+create table gst_gasto (
+  gst_id bigint unsigned auto_increment,
+  gst_valor numeric(10,2) not null,
+  gst_descricao varchar(100),
+  primary key (gst_id)
+);
+
+create table lnc_lance (
+  lnc_id bigint unsigned auto_increment,
+  lnc_data_hora datetime not null,
+  lnc_jogador varchar(50) not null,
+  lnc_descricao varchar(200) not null,
+  primary key (lnc_id)
+);
+
+create table lcl_localidade (
+  lcl_id bigint unsigned auto_increment,
+  lcl_latitude numeric(10,8) not null,
+  lcl_longitude numeric(10,8) not null,
+  lcl_descricao varchar(200) not null,
+  primary key (lcl_id),
+  unique (lcl_latitude, lcl_longitude)
+);
+
 insert into usr_usuario (usr_nome, usr_senha)
-    values ('admin', '$2a$10$i3.Z8Yv1Fwl0I5SNjdCGkOTRGQjGvHjh/gMZhdc3e7LIovAklqM6C');
+  values ('admin', '$2a$10$i3.Z8Yv1Fwl0I5SNjdCGkOTRGQjGvHjh/gMZhdc3e7LIovAklqM6C');
 insert into aut_autorizacao (aut_nome)
-    values ('ROLE_ADMIN');
+  values ('ROLE_ADMIN');
 insert into uau_usuario_autorizacao values (1, 1);
 insert into ant_anotacao(ant_texto, usr_id) 
-    values ('Teste', 1);
+  values ('Teste', 1);
+insert into gst_gasto (gst_valor, gst_descricao)
+  values (10.20, 'Sorvete');
+insert into lnc_lance (lnc_data_hora, lnc_jogador, lnc_descricao)
+  values ('2022-11-29 11:00', 'Paul', 'Errou!');
+insert into lcl_localidade (lcl_latitude, lcl_longitude, lcl_descricao)
+  values (20.0741, -15.1020, 'Algo');
